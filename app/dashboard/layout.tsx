eea0350595007f900d777ff49ec98997c30136e2
saadhaq70 <mdsaad1542@example.com>
2026-06-21 05:37:28 +0530

    sv

--- app/dashboard/layout.tsx
+++ app/dashboard/layout.tsx
@@ -2,15 +2,21 @@ import { SidebarProvider } from "@/components/ui/sidebar";
 import { getAllPlaygroundForUser } from "@/modules/dashboard/actions";
 import { DashboardSidebar } from "@/modules/dashboard/components/dashboard-sidebar";
 
+type PlaygroundItem = {
+  id: string;
+  title: string;
+  template: string;
+  Starmark?: Array<{ isMarked: boolean }>;
+};
+
 export default async function DashboardLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
+  const playgroundData = await getAllPlaygroundForUser();
 
-    const playgroundData = await getAllPlaygroundForUser();
-
-    console.log("playgroundData", playgroundData);
+  console.log("playgroundData", playgroundData);
 
   const technologyIconMap: Record<string, string> = {
     REACT: "Zap",
@@ -19,13 +25,6 @@ export default async function DashboardLayout({
     VUE: "Compass",
     HONO: "FlameIcon",
     ANGULAR: "Terminal",
-  }
-
-  type PlaygroundItem = {
-    id: string;
-    title: string;
-    template: string;
-    Starmark?: Array<{ isMarked: boolean }>;
   };
 
   const formattedPlaygroundData = playgroundData?.map((item: PlaygroundItem) => ({
@@ -33,19 +32,15 @@ export default async function DashboardLayout({
     name: item.title,
     starred: item.Starmark?.[0]?.isMarked || false,
     icon: technologyIconMap[item.template] || "Code2"
-  }))
-
+  }));
 
   return (
-
-  <SidebarProvider>
-    
-    <div className="flex min-h-screen w-full overflow-x-hidden">
-      {/* Dashboard Sidebar */}
-      <DashboardSidebar initialPlaygroundData={formattedPlaygroundData}/>
-      <main className="flex-1">{children}</main>
-    </div>
-  </SidebarProvider>
-  )
-
+    <SidebarProvider>
+      <div className="flex min-h-screen w-full overflow-x-hidden">
+        {/* Dashboard Sidebar */}
+        <DashboardSidebar initialPlaygroundData={formattedPlaygroundData}/>
+        <main className="flex-1">{children}</main>
+      </div>
+    </SidebarProvider>
+  );
 }
